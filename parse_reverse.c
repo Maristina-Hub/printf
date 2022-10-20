b4903b79457a62315690460b9d4755472ff028e1
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-10-20 23:55:04 +0100

    Update parse_reverse.c

--- parse_reverse.c
+++ parse_reverse.c
@@ -8,20 +8,20 @@
  */
 int parse_rev(char *buff_dest, va_list arg, int buff_count)
 {
-	int i = 0;
-	char *str;
-	str = va_arg(arg, char *);
-	if (str)
-	{
-		while (str[i] != '\0')
-		i++;
-		i = i - 1;
-		while (i >= 0)
-		{
-			buff_dest[buff_count] = str[i];
-			buff_count++;
-			i--;
-		}
-	}
-	return (buff_count);
+int i = 0;
+char *str;
+str = va_arg(arg, char *);
+if (str)
+{
+while (str[i] != '\0')
+i++;
+i = i - 1;
+while (i >= 0)
+{
+buff_dest[buff_count] = str[i];
+buff_count++;
+i--;
+}
+}
+return (buff_count);
 }
